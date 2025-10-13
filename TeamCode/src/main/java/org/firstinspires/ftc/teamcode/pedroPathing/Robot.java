030be9dee1c9d170828635a264b6be6ad72f1020
Freelandia <dash.duan@outlook.com>
2025-10-13 12:53:08 -0400

    Added robot class to code, still things to do with constants and teleop.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Robot.java
@@ -0,0 +1,134 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+public class Robot {
+    DcMotor frontLeftMotor;
+    DcMotor backLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backRightMotor;
+    DcMotor intakeMotor;
+    DcMotor shooterMotor;
+
+    Servo ballKicker;
+    Servo adjustHood;
+
+    IMU imu;
+
+    final int TICKS_PER_INCH = 45; // 11.87 in per rev; 537.7 ticks per rev; 537.7/11.87 ticks per inch
+
+    int leftFrontPos = 0;
+    int leftBackPos = 0;
+    int rightFrontPos = 0;
+    int rightBackPos = 0;
+
+    private LinearOpMode myOpMode;
+    public Robot(LinearOpMode opMode){
+        this.myOpMode = opMode;
+    }
+
+    public void initHardware(){
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        frontLeftMotor = myOpMode.hardwareMap.dcMotor.get("frontLeftMotor");
+        backLeftMotor = myOpMode.hardwareMap.dcMotor.get("backLeftMotor");
+        frontRightMotor = myOpMode.hardwareMap.dcMotor.get("frontRightMotor");
+        backRightMotor = myOpMode.hardwareMap.dcMotor.get("backRightMotor");
+        intakeMotor = myOpMode.hardwareMap.dcMotor.get("intakeMotor");
+        shooterMotor = myOpMode.hardwareMap.dcMotor.get("shooterMotor");
+
+        ballKicker = myOpMode.hardwareMap.servo.get("ballKicker");
+        adjustHood = myOpMode.hardwareMap.servo.get("adjustHood");
+
+        /*
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+         */
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        shooterMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        initIMU();
+    }
+
+    public void robotMove(double frontLeftPower, double backLeftPower, double frontRightPower, double backRightPower){
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
+    }
+
+    public void robotBrakeBehavior(){
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        shooterMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void drive(int lF, int lB, int rF, int rB, double speed) {
+
+        leftFrontPos -= lF;
+        leftBackPos -= lB;
+        rightFrontPos -= rF;
+        rightBackPos -= rB;
+
+        frontLeftMotor.setTargetPosition(leftFrontPos);
+        backLeftMotor.setTargetPosition(leftBackPos);
+        frontRightMotor.setTargetPosition(rightFrontPos);
+        backRightMotor.setTargetPosition(rightBackPos);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        double driftFactor = 1.042;
+        frontLeftMotor.setPower(speed);
+        backLeftMotor.setPower(speed);
+        frontRightMotor.setPower(speed);
+        backRightMotor.setPower(speed);
+
+        waitDrive();
+    }
+
+    private void waitDrive() {
+        while (frontLeftMotor.isBusy() && frontRightMotor.isBusy() && backLeftMotor.isBusy() && backRightMotor.isBusy() && myOpMode.opModeIsActive()) ;
+    }
+
+    private void initIMU() {
+        // Retrieve the IMU from the hardware map
+        imu = myOpMode.hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+    }
+}
